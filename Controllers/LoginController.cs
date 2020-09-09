f6794ba0e880ea76790355b5f6f22e3ff8de9c50
Samson Akanbi <samson.akanbi@outlook.com>
2020-09-09 22:44:21 +0100

    Created a self token in appsettings.json

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JWTAuthDemo.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace JWTAuthDemo.Controllers
 {
@@ -11,5 +13,43 @@ namespace JWTAuthDemo.Controllers
     [ApiController]
     public class LoginController : ControllerBase
     {
+        private IConfiguration _config;
+
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string username, string pass)
+        {
+            UserModel login = new UserModel();
+            login.UserName = username;
+            login.Password = pass;
+            IActionResult response = Unauthorized();
+
+            var user = AuthenticateUser(login);
+
+            if(user != null)
+            {
+                var tokenStr = GenerateJSONWebToken(user);
+                response = Ok(new { token = tokenStr });
+            }
+        }
+
+        private UserModel AuthenticateUser(UserModel login)
+        {
+            UserModel user = null;
+            if(login.UserName == "samseen" && login.Password == "123")
+            {
+                user = new UserModel { UserName = "Samseen", EmailAddress = "developersamseen@outlook.com", Password = "123" };
+            }
+            return user;
+        }
+
+        private string GenerateJSONWebToken(UserModel userinfo)
+        {
+            var securityKey = new SymmetricSecurityKey();
+        }
     }
 }
